59a3d8e57729465690bf638a057339b9b12dd8df
Trevor Ford <info@trvrfrd.net>
2018-04-14 12:02:08 -0400

    config webpack + babel + some other boilerplate-y stuff

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/App.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
