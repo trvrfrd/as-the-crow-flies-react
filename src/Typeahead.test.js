804e75b8582016530714c0573f1eaaf16932bc93
Trevor Ford <info@trvrfrd.net>
2018-07-28 19:51:06 -0400

    add a couple more tests for basic suggestions + reorganize

--- src/Typeahead.test.js
+++ src/Typeahead.test.js
@@ -2,13 +2,6 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Typeahead from './Typeahead';
 
-const formatFunction = data => data.text;
-const source = [
-  { text: 'apple' },
-  { text: 'banana' },
-  { text: 'cherry' }
-];
-
 describe('Typeahead', () => {
 
   test('basic rendering snapshot', () => {
@@ -16,22 +9,51 @@ describe('Typeahead', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-
-  test('shows suggestion when there is a single match', () => {
+  describe('basic suggestions', () => {
+    const formatFunction = data => data.text;
+    const source = [
+      { text: 'apple' },
+      { text: 'banana' },
+      { text: 'cherry' }
+    ];
     const onSelect = () => { };
-    const wrapper = mount(
-      <Typeahead
-        formatSuggestion={formatFunction}
-        source={source}
-        onSelect={onSelect}
-      />
-    );
-    const input = wrapper.find('input');
+    let wrapper;
+    let input;
 
-    input.simulate('change', { target: { value: 'c' } });
+    beforeEach(() => {
+      wrapper = mount(
+        <Typeahead
+          formatSuggestion={formatFunction}
+          source={source}
+          onSelect={onSelect}
+        />
+      );
+      input = wrapper.find('input');
+    });
 
-    expect(wrapper.find('.suggestion')).toHaveLength(1);
-    expect(wrapper.find('.suggestion').text()).toBe('cherry');
+    afterEach(() => wrapper.unmount());
+
+    test('shows suggestion when there is a single match', () => {
+      input.simulate('change', { target: { value: 'c' } });
+
+      expect(wrapper.find('.suggestion')).toHaveLength(1);
+      expect(wrapper.find('.suggestion').text()).toBe('cherry');
+    });
+
+    test('shows suggestions when there are multiple matches', () => {
+      input.simulate('change', { target: { value: 'a' } });
+      expect(wrapper.find('.suggestion')).toHaveLength(2);
+      expect(wrapper.find('.suggestion').first().text()).toBe('apple');
+      expect(wrapper.find('.suggestion').last().text()).toBe('banana');
+    });
+
+    test('maxSuggestions prop limits number of suggestions', () => {
+      wrapper.setProps({ maxSuggestions: 1 });
+      input.simulate('change', { target: { value: 'a' } });
+      expect(wrapper.find('.suggestion')).toHaveLength(1);
+      expect(wrapper.find('.suggestion').text()).toBe('apple');
+    });
+
   });
 
 });
