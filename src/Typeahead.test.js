ddb819bc29f274e00ca5dc51c3b9580801d67518
Trevor Ford <info@trvrfrd.net>
2018-07-28 19:31:59 -0400

    add an actual basic Typeahead test! and nest tests under describe()

--- src/Typeahead.test.js
+++ src/Typeahead.test.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Typeahead from './Typeahead';
 
-const formatFunction = JSON.stringify;
+const formatFunction = data => data.text;
+const source = [
+  { text: 'apple' },
+  { text: 'banana' },
+  { text: 'cherry' }
+];
 
-test('Typeahead basic rendering snapshot', () => {
-  const wrapper = shallow(<Typeahead />);
-  expect(wrapper).toMatchSnapshot();
+describe('Typeahead', () => {
+
+  test('basic rendering snapshot', () => {
+    const wrapper = shallow(<Typeahead />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+
+  test('shows suggestion when there is a single match', () => {
+    const onSelect = () => { };
+    const wrapper = mount(
+      <Typeahead
+        formatSuggestion={formatFunction}
+        source={source}
+        onSelect={onSelect}
+      />
+    );
+    const input = wrapper.find('input');
+
+    input.simulate('change', { target: { value: 'c' } });
+
+    expect(wrapper.find('.suggestion')).toHaveLength(1);
+    expect(wrapper.find('.suggestion').text()).toBe('cherry');
+  });
+
 });
