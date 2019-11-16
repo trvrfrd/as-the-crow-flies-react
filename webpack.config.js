3401c524c4e824850efe4ad63ca5261a32e056af
Trevor Ford <info@trvrfrd.net>
2019-11-16 09:14:17 -0500

    call CleanWebPackplugin correctly for v2 in webpack.config.js how do i make CI catch this stuff L.O.L.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -36,7 +36,7 @@ module.exports = {
   },
 
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html')
     })
